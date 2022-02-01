0cea3e3b218a4c36c3193875b384875ec5b01ed6
blq200 <92025169+blq200@users.noreply.github.com>
2022-02-01 08:12:09 +0000

    Update main.js

--- produitJson/main.js
+++ produitJson/main.js
@@ -1,5 +1,5 @@
 
-       var tbody = document.getElementsByTagName("tbody")[0];
+      var tbody = document.getElementsByTagName("tbody")[0];
       var DATA;
 //Créer un fichier produits.json qui contient une dizaine de produits
         $.ajax({
@@ -34,36 +34,35 @@
             $("tbody").html("");
             fill(list);
         })
-// //trie 
-//         $(function() {
-//           $("#prix").on('click', function() {
-//               var rows = $("#table tbody  tr").get();
-//               rows.sort(sortTable);
-//               $.each(rows, function(index, row) {
-//                   $("#table ").children("tbody").append(row);
-//               });
-//               if (ascending) {
-//                   ascending = false;
-//               } else {
-//                   ascending = true;
-//               }
-//           });
-//       });
+//L'option de tri ascendant et descendant sur toutes les colonnes.
+        $(function() {
+          $("#table").on('click', function() {
+              var rows = $("#table tbody  tr").get();
+              rows.sort(sortTable);
+              $.each(rows, function(index, row) {
+                  $("#table ").children("tbody").append(row);
+              });
+              if (ascending) {
+                  ascending = false;
+              } else {
+                  ascending = true;
+              }
+          });
+      });
       
-//       var ascending = false;
+      var ascending = false;
       
-//       function sortTable(a, b) {
-//           var A = parseInt($(a).children('td').eq(0).text(), 10);
-//           var B = parseInt($(b).children('td').eq(0).text(), 10);
+      function sortTable(a, b) {
+          var A = parseInt($(a).children('td').eq(0).text(), 10);
+          var B = parseInt($(b).children('td').eq(0).text(), 10);
       
       
-//           if (ascending) {
-//               if (A > B) return 1;
-//               if (A < B) return -1;
-//           } else {
-//               if (A > B) return -1;
-//               if (A < B) return 1;
-//           }
-//           return 0;
-//       }
-      
+          if (ascending) {
+              if (A > B) return 1;
+              if (A < B) return -1;
+          } else {
+              if (A > B) return -1;
+              if (A < B) return 1;
+          }
+          return 0;
+      }
